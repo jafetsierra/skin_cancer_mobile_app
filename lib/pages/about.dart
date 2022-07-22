4004d6fd26cc94d939b89ddfa632f846c193b457
jafet <jafet.sierra.l@gmail.com>
2022-07-22 17:38:06 -0500

    result page changed with a message for the result

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -41,38 +41,6 @@ class _AboutPageState extends State<AboutPage> {
               //*************appbar
               CustomAppBar(primaryColor: widget.primaryColor, secondaryColor: widget.secondaryColor, auxColor: widget.auxColor,textcolor: widget.textColor,),
               const SizedBox(height: 30,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal:20.0),
-                child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(color: widget.primaryColor, boxShadow: [
-                      BoxShadow(
-                        color: widget.auxColor,
-                        spreadRadius: 1,
-                        blurRadius: 1,
-                        offset: const Offset(0, 7), // changes position of shadow
-                        ),
-                    ],
-                    borderRadius: BorderRadius.circular(10)),
-                    height: 315,
-                    child: Column(
-                      children: [
-                        const Text('Types of lesions',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
-                        const SizedBox(height: 10,),
-                        Image.asset('lib/images/dic.png',height: 180,),
-                        const SizedBox(height: 5,),
-                        Text("The model is able to diagnose 7 diferent types of skin lesions. Even though it has an accuracy above 90% it's just for prevention. Do not use as a final diagnose and pay your demartologist a visit in case of suspicion of cancer.",
-                            style: TextStyle(color: Colors.white),),
-                        // Text('Checkout the official repository for a better insight and specific information',
-                        //   style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: widget.textColor),
-                        //   overflow: TextOverflow.ellipsis,
-                        // )
-
-                      ],
-                    ),
-                ),
-              ),
-
               Padding(padding: EdgeInsets.all(10),
                 child: Container(
                   child: Text('The API was build using transfer learning techique with RestNet50 V2 as the base model. For further information please go to the official repository at -> ',
--- lib/pages/resultPage.dart
+++ lib/pages/resultPage.dart
@@ -38,10 +38,42 @@ class _ResultPageState extends State<ResultPage> {
           ),
         child: 
           Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              CustomAppBar(primaryColor: widget.primaryColor, secondaryColor: widget.secondaryColor, auxColor: widget.auxColor,textcolor: widget.textColor,),
               Text(widget.rta,
               style: TextStyle(color: widget.textColor),),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal:20.0),
+                child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(color: widget.primaryColor, boxShadow: [
+                      BoxShadow(
+                        color: widget.auxColor,
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                        offset: const Offset(0, 7), // changes position of shadow
+                        ),
+                    ],
+                    borderRadius: BorderRadius.circular(10)),
+                    height: 315,
+                    child: Column(
+                      children: [
+                        const Text('Types of lesions',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                        const SizedBox(height: 10,),
+                        Image.asset('lib/images/dic.png',height: 180,),
+                        const SizedBox(height: 5,),
+                        Text("The model is able to diagnose 7 diferent types of skin lesions. Even though it has an accuracy above 90% it's just for prevention. Do not use as a final diagnose and pay your demartologist a visit in case of suspicion of cancer.",
+                            style: TextStyle(color: Colors.white),),
+                        // Text('Checkout the official repository for a better insight and specific information',
+                        //   style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: widget.textColor),
+                        //   overflow: TextOverflow.ellipsis,
+                        // )
+
+                      ],
+                    ),
+                ),
+              ),
               GoDiagnosisPage(primaryColor: widget.primaryColor, secondaryColor: widget.secondaryColor, auxColor: widget.auxColor, textColor: widget.textColor),
               GoHomePage(primaryColor: widget.primaryColor, secondaryColor: widget.secondaryColor, auxColor: widget.auxColor, textColor: widget.textColor),
             ],
